2ca4d47f6cdf9b1d1823e22bd1e878ce418499fe
Giovani Trierweiler <giovanitrier@gmail.com>
2025-08-09 17:57:06 -0300

    Atualização Login

--- sitema-controle-presenca/src/app/app.routes.ts
+++ sitema-controle-presenca/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LoginComponent } from './pages/login/login';
 
-export const routes: Routes = [];
+//Definição das rotas da aplicação
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
---
+++ sitema-controle-presenca/src/app/pages/login/login.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'; //Define a clase como componente Angular.
+import { CommonModule } from '@angular/common'; // Importa diretivas e pipes como *ngIf, *ngFor.
+import { FormsModule } from '@angular/forms'; // Habilita o uso de formulários baseados em template, incluindo [(ngModel)] para two-way binding
+import { Login } from '../../services/models/login.model'; //Importa a interface Login
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+
+export class LoginComponent {
+
+  login: Login = new Login();
+
+  onSubmit() {
+    console.log('Dados do login:', this.login);
+  }
+}
