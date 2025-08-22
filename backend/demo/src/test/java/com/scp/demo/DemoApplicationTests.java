9c5a185d20612e68b5e3cbdf3cbf1aa51f2f1c3b
aruniamstel <bioarui@gmail.com>
2025-08-22 12:45:29 -0300

    criação do backend

---
+++ backend/demo/src/test/java/com/scp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
