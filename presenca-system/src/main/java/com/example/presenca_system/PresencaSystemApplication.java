8fa43efa216333d6ed3ad1c19052883e5616b9f1
zerouacuca <sdo23lucas@gmail.com>
2025-08-24 12:22:39 -0300

    feat: criacao da backend

---
+++ presenca-system/src/main/java/com/example/presenca_system/PresencaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.presenca_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresencaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresencaSystemApplication.class, args);
+	}
+
+}
