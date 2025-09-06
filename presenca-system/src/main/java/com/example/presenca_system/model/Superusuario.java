2852916d5e7cc5799e736ada0e9d362096bac589
zerouacuca <sdo23lucas@gmail.com>
2025-09-06 14:30:25 -0300

    feat: Superusuario.java - alteração relação de herança para composição

--- presenca-system/src/main/java/com/example/presenca_system/model/Superusuario.java
+++ presenca-system/src/main/java/com/example/presenca_system/model/Superusuario.java
@@ -1,24 +1,33 @@
 package com.example.presenca_system.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @Table(name = "superusuarios")
-@PrimaryKeyJoinColumn(name = "cpf")
-@EqualsAndHashCode(callSuper = true)
-public class Superusuario extends Usuario {
+public class Superusuario {
+
+    @Id
+    @Column(name = "cpf")
+    private String cpf;
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "senha", nullable = false)
     private String senha;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_cpf", referencedColumnName = "cpf")
+    private Usuario usuario;
 
 }
