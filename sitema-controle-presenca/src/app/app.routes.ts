fb55d1fa654bbca46190745194fc35c04ee0dcb7
sinapsense <sdo23lucas@gmail.com>
2025-09-30 14:10:07 -0300

    integracao branch front-end

--- sitema-controle-presenca/src/app/app.routes.ts
+++ sitema-controle-presenca/src/app/app.routes.ts
@@ -4,31 +4,33 @@ import { TabelaEventos } from './pages/tabela-eventos/tabela-eventos';
 import { EventoCrud } from './pages/evento-crud/evento-crud';
 import { TabelaUsuarios } from './pages/tabela-usuarios/tabela-usuarios';
 import { UsuarioCrud } from './pages/usuario-crud/usuario-crud';
-import { Navbar } from './componentes/navbar/navbar';
-import { Usuarios } from './pages/usuarios/usuarios';
-import { UsuarioCadastro } from './pages/usuario-cadastro/usuario-cadastro';
 import { CertificadosComponent } from './pages/certificados/certificados.component';
 import { DetalhesEventoComponent } from './pages/detalhes-evento/detalhes-evento.component';
-
-// Definição das rotas da aplicação
+import { PaginaPrincipal } from './pages/pagina-principal/pagina-principal';
+import { Navbar } from './pages/navbar/navbar';
 export const routes: Routes = [
-  // Rotas específicas devem vir primeiro
-  { path: 'login', component: LoginComponent },
-  { path: 'eventos', component: TabelaEventos },
-  { path: 'cadastrar-evento', component: EventoCrud },
-  { path: 'tabela-usuarios', component: TabelaUsuarios },
-  { path: 'editar-usuario', component: UsuarioCrud },
-  { path: 'navbar', component: Navbar },
-  { path: 'usuarios', component: Usuarios },
-  { path: 'cadastrar-usuario', component: UsuarioCadastro },
-  { path: 'certificados', component: CertificadosComponent },
-  
-  // Rotas de edição de usuário com parâmetro CPF
-  { path: 'editar-usuario/:cpf', component: UsuarioCrud },
-  // Rotas de edição de evento com parâmetro ID
-  { path: 'editar-evento/:id', component: EventoCrud },
-  { path: 'detalhes-evento/:id', component: DetalhesEventoComponent },
 
-  // A rota de redirecionamento ou wildcard deve sempre vir por último
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+
+
+  {
+    path: '',
+    component: PaginaPrincipal,
+    children: [
+      {path: 'navbar', component: Navbar},
+      { path: 'eventos', component: TabelaEventos },
+      { path: 'cadastrar-evento', component: EventoCrud },
+      { path: 'editar-evento/:id', component: EventoCrud },
+      { path: 'detalhes-evento/:id', component: DetalhesEventoComponent },
+      { path: 'tabela-usuarios', component: TabelaUsuarios },
+      { path: 'cadastrar-usuario', component: UsuarioCrud },
+      { path: 'editar-usuario', component: UsuarioCrud },
+      { path: 'editar-usuario/:cpf', component: UsuarioCrud },
+      { path: 'certificados', component: CertificadosComponent },
+    ]
+  },
+
+  // Wildcard
+  { path: '**', redirectTo: '/login' },
+
 ];
